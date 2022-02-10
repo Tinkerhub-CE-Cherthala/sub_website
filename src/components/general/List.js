496a5b5d0d644e1c8a7c79da3cd7904c89486bb5
neerajram30 <neerajramachandran30@gmail.com>
2022-02-10 23:26:38 +0530

    #rzr

--- src/components/general/List.js
+++ src/components/general/List.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React,{useEffect} from "react";
 import {Link} from 'react-router-dom'
 import header from './images/header-img.png'
 import img1 from './images/img1.jpg'
 import './List.css'
 
 function List(){
+
+    useEffect(()=>{
+        const Script = document.createElement("script");
+        //id should be same as given to form element
+        const Form = document.getElementById('donateForm');
+        Script.setAttribute('src','https://checkout.razorpay.com/v1/payment-button.js')
+        Script.setAttribute('data-payment_button_id','pl_IuMZYnGI4GpJ0u')
+        Form.appendChild(Script);
+      },[])
+
+
     return(
         <div>
 
@@ -25,16 +36,16 @@ function List(){
         <div class="info">
             <h3>EVENT1</h3>
             <p>DETAILS</p>
-            <a class="btn">Register</a>
+            <form id="donateForm"></form>
         </div>
     </div>
-
+    
     <div class="box">
         <img src="" alt=""/>
         <div class="info">
             <h3>EVENT2</h3>
             <p>details</p>
-            <a href="http://127.0.0.1:5500/front/src/form/index.html" class="btn">REGISTER</a>
+            <a href="https://rzp.io/l/UmDMW1zN" target="_blank" class="btn">REGISTER</a>
         </div>
     </div>
 
