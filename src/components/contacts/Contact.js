8ab7839e8a43297847dfbc60907514039d73ab34
neerajram30 <neerajramachandran30@gmail.com>
2022-03-07 10:39:09 +0530

    Contact page

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Contact.css';
+import Contactcss from './Contact.module.css';
 
 function map(){
     return(
@@ -9,79 +9,79 @@ function map(){
 
 export default function Contact() {
     return (
-        <section class="contact">
-        <div class="content">
+        <section class={Contactcss.contact}>
+        <div class={Contactcss.content}>
             <h2>Contact Us</h2>
         </div>
-        <div class="container">
-            <div class="contactinfo">
-                <div class="phonere" onclick="window.open('tel:+917306795119');">
-                     <div class="box">
-                         <div class="icon"><i class="fas fa-phone"></i></div>
+        <div class={Contactcss.container}>
+            <div class={Contactcss.contactinfo}>
+                <div class={Contactcss.phonere} onclick="window.open('tel:+917306795119');">
+                     <div class={Contactcss.box}>
+                         <div class={Contactcss.icon}><i class="fas fa-phone"></i></div>
                      </div>
-                     <div class="text">
-                        <a href="tel:+917306795119" class="contact-data">+91 7306795119</a>
+                     <div class={Contactcss.text}>
+                        <a href="tel:+917306795119" class={Contactcss.contactdata}>+91 7306795119</a>
                      </div>
                 </div>
-                <div class="mailre" onclick="window.open('mailto:jayakrishnans2041@gmail.com');">
-                    <div class="box">
-                        <div class="icon"><i class="fas fa-envelope"></i></div>
+                <div class={Contactcss.mailre} onclick="window.open('mailto:jayakrishnans2041@gmail.com');">
+                    <div class={Contactcss.box}>
+                        <div class={Contactcss.icon}><i class="fas fa-envelope"></i></div>
                     </div>
-                    <div class="text">
-                        <a href="mailto:jayakrishnans2041@gmail.com" class="mail-data">jayakrishnans2041@gmail.com</a>
+                    <div class={Contactcss.text}>
+                        <a href="mailto:jayakrishnans2041@gmail.com" class={Contactcss.maildata}>jayakrishnans2041@gmail.com</a>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="icon">
+                <div class={Contactcss.box}>
+                    <div class={Contactcss.icon}>
                         <i class="fas fa-map-marker-alt"></i>
                     </div>
-                    <div class="marker" src={map()}>
+                    <div class={Contactcss.marker} src={map()}>
                        
                     </div>
                 </div>
-                <div class="smedia">
-                    <div class="smedia">
-                        <div class="facebook">
-                            <a href="https://www.facebook.com/ceczealous/" class="icon-link"><i class="fab fa-facebook"></i></a>
+                <div class={Contactcss.smedia}>
+                    <div class={Contactcss.smedia}>
+                        <div class={Contactcss.facebook}>
+                            <a href="https://www.facebook.com/ceczealous/" class={Contactcss.iconlink}><i class="fab fa-facebook"></i></a>
                         </div>
                     </div>
-                    <div class="smedia">
+                    <div class={Contactcss.smedia}>
                         <div class="instagram">
-                            <a href="https://www.instagram.com/zealouscec/?hl=en" class="icon-link"><i class="fab fa-instagram" id="insta"></i></a>
+                            <a href="https://www.instagram.com/zealouscec/?hl=en" class={Contactcss.iconlink}><i class="fab fa-instagram" id="insta"></i></a>
                         </div>
                     </div>
-                    <div class="smedia">
-                        <div class="youtube">
-                            <a href="https://www.youtube.com/channel/UCjNm_V23UkeU3lzFT_fcTiA" class="icon-link"><i class="fab fa-youtube" id="yout"></i></a>
+                    <div class={Contactcss.smedia}>
+                        <div class={Contactcss.youtube}>
+                            <a href="https://www.youtube.com/channel/UCjNm_V23UkeU3lzFT_fcTiA" class={Contactcss.iconlink}><i class="fab fa-youtube" id="yout"></i></a>
                         </div>
                     </div>    
-                    <div class="smedia">
-                        <div class="twitter">
+                    <div class={Contactcss.smedia}>
+                        <div class={Contactcss.twitter}>
                             <a href="https://twitter.com/zealouscec" class="icon-link"><i class="fab fa-twitter"></i></a>
                         </div>
                     </div>        
                 </div>
             </div>
-            <div class="contactform">
+            <div class={Contactcss.contactform}>
                 <h2>Send Message</h2>
                 <form action="" method="post">
-                    <div class="inputbox">
+                    <div class={Contactcss.inputbox}>
                         <input type="text" name="" required="required" />
                         <span>Full Name</span>
                     </div>
-                    <div class="inputbox">
+                    <div class={Contactcss.inputbox}>
                         <input type="text" name="" required="required" />
                         <span>Phone Number</span>
                     </div>
-                    <div class="inputbox">
+                    <div class={Contactcss.inputbox}>
                         <input type="email" name="" required="required" />
                         <span>Email</span>
                     </div>
-                    <div class="inputbox">
+                    <div class={Contactcss.inputbox}>
                         <textarea required="required"></textarea>
                         <span>Type your Message</span>                        
                     </div>
-                    <div class="inputbox" >
+                    <div class={Contactcss.inputbox} >
                         <button type="submit">Send</button>
                     </div>
                 </form> 
