efd24558a28cdf531f5b693de1d007b87cf45aad
neerajram30 <neerajramachandran30@gmail.com>
2022-03-15 00:18:20 +0530

    ##about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,10 +4,11 @@ import Aboutcss from './About.module.css'
 
 function About() {
   return (
-    <div >
-        <h2><u>About</u></h2>
+    <div className={Aboutcss.full}>
+    <div className={Aboutcss.cardm}>
+        <h2 className={Aboutcss.heading}><u>About</u></h2>
         <p className={Aboutcss.para}>Back from 2014,the College of Engineering Cherthala annually organizes TECHFEST with an aim to promote innovations of budding engineers to explore and exploit diverse talents in various spheres.ZEALOUS,the so called technical event,makes it almost like a modern-day Athens,the place of Athena herself.</p>
-        <h2><u>Our Sponcers</u></h2> 
+        <h2 ><u>Our Sponsors</u></h2> 
         <div className={Aboutcss.container}>
     <input type="radio" name="dot" id="one"/>
     <input type="radio" name="dot" id="two"/>
@@ -113,8 +114,8 @@ function About() {
         </div>
       </div>
     </div>
-    <div class="button">
-      <label for="one" class=" active one"></label>
+    <div class={Aboutcss.button}>
+      <label for="one" class="active one"></label>
       <label for="two" class="two"></label>
     </div>
   </div>
@@ -123,18 +124,18 @@ function About() {
   
   
   <h2><u>Our Committee</u></h2> 
-  <div class="container">
-  <div class="row">
+  <div class={Aboutcss.container}>
+  <div class={Aboutcss.row}>
     <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-      <div class="our-team">
-        <div class="picture">
-          <img class="img-fluid" src="https://picsum.photos/130/130?image=1027"/>
+      <div class={Aboutcss.ourteam}>
+        <div class={Aboutcss.picture}>
+          <img class={Aboutcss.imgfluid} src="https://picsum.photos/130/130?image=1027"/>
         </div>
-        <div class="team-content">
-          <h3 class="name">Anjana</h3>
-          <h4 class="title">Member</h4>
+        <div class={Aboutcss.teamcontent}>
+          <h3 class={Aboutcss.name}>Anjana</h3>
+          <h4 class={Aboutcss.title}>Member</h4>
         </div>
-        <ul class="social">
+        <ul class={Aboutcss.social}>
           <li><a href="tel:+1234567" class="fa fa-phone" aria-hidden="true"></a></li>
           <li><a href="testmail@gmail.com" class="fa fa-envelope" aria-hidden="true"></a></li>
         
@@ -142,28 +143,28 @@ function About() {
       </div>
     </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-      <div class="our-team">
-        <div class="picture">
-          <img class="img-fluid" src="https://picsum.photos/130/130?image=839"/>
+      <div class={Aboutcss.ourteam}>
+        <div class={Aboutcss.picture}>
+          <img class={Aboutcss.imgfluid} src="https://picsum.photos/130/130?image=839"/>
         </div>
-        <div class="team-content">
-          <h3 class="name">Ajith</h3>
-          <h4 class="title">Web Developer</h4>
+        <div class={Aboutcss.teamcontent}>
+          <h3 class={Aboutcss.name}>Ajith</h3>
+          <h4 class={Aboutcss.title}>Web Developer</h4>
         </div>
-        <ul class="social">
+        <ul class={Aboutcss.social}>
           <li><a href="tel:+1234567" class="fa fa-phone" aria-hidden="true"></a></li>
           <li><a href="testmail@gmail.com" class="fa fa-envelope" aria-hidden="true"></a></li>
         </ul>
       </div>
     </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-      <div class="our-team">
-        <div class="picture">
-          <img class="img-fluid" src="https://picsum.photos/130/130?image=856"/>
+      <div class={Aboutcss.ourteam}>
+        <div class={Aboutcss.picture}>
+          <img class={Aboutcss.imgfluid} src="https://picsum.photos/130/130?image=856"/>
         </div>
-        <div class="team-content">
-          <h3 class="name">Akhil</h3>
-          <h4 class="title">Web Developer</h4>
+        <div class={Aboutcss.teamcontent}>
+          <h3 class={Aboutcss.name}>Akhil</h3>
+          <h4 class={Aboutcss.title}>Web Developer</h4>
         </div>
         <ul class={Aboutcss.social}>
           <li><a href="tel:+1234567" class="fa fa-phone" aria-hidden="true"></a></li>
@@ -188,6 +189,7 @@ function About() {
     </div>
   </div>
 </div>
+    </div>
     </div>
   )
 }
